7e05f40b8e238a00b50ab97a4849efe733a843c6
itsuki <itsuki.enjyoy@gmail.com>
2025-02-26 09:46:46 +0900

    Initial Commit

---
+++ CognitoDemo/CognitoDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CognitoDemoApp.swift
+//  CognitoDemo
+//
+//  Created by Itsuki on 2025/02/26.
+//
+
+import SwiftUI
+
+@main
+struct CognitoDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
